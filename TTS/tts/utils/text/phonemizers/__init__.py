5a65010ec3d1e416e6ff01fbdac7229e7b179b26
Kelvin Fiifi Nketia-Achiampong <achiampongk22@gmail.com>
2025-03-14 14:51:18 +0000

    feat: implement akan  frontend

--- TTS/tts/utils/text/phonemizers/__init__.py
+++ TTS/tts/utils/text/phonemizers/__init__.py
@@ -26,6 +26,7 @@ except ImportError:
 try:
     from TTS.tts.utils.text.phonemizers.akan_phonemizer import AKA_Phonemizer
 except ImportError:
+    print("Unable to import")
     AKA_Phonemizer = None
 
 PHONEMIZERS = {b.name(): b for b in (ESpeak, Gruut)}
